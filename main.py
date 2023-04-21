50fdc1ba39e8abd6a6228dac52375ebd0ddcde92
Adam Washington <adam.washington@stfc.ac.uk>
2023-04-21 17:01:11 +0100

    Move model into qml

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import numpy as np
 from PySide6.QtCore import QObject, Signal, Property, Slot
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQuick import QQuickView
-from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
 
 class Model(QObject):
     modelChanged = Signal()
 
+    m_slope = 0.0
+    m_intercept = 0.0
+    m_xrange = (-5.0, 5.0)
+
     def __init__(self):
         QObject.__init__(self)
-        self.m_slope = 0.0
-        self.m_intercept = 0.0
+        self.m_xs = np.linspace(self.m_xrange[0], self.m_xrange[1], 100)
+        self.m_ys = self.m_xs *self.m_slope + self.m_intercept
 
     @Property(float, notify=modelChanged)
     def slope(self):
@@ -29,19 +33,28 @@ class Model(QObject):
     @intercept.setter
     def intercept(self, val):
         self.m_intercept = val
+        print(val)
         self.modelChanged.emit()
 
-    @Slot()
-    def on_currentValueChanged(self):
-        print(self.m_currentValue)
+    @Property(float, notify=modelChanged)
+    def y_min(self):
+        self.m_xs = np.linspace(self.m_xrange[0], self.m_xrange[1], 100)
+        self.m_ys = self.m_xs *self.m_slope + self.m_intercept
+        return np.min(self.m_ys)
+
+    @Property(float, notify=modelChanged)
+    def y_max(self):
+        self.m_xs = np.linspace(self.m_xrange[0], self.m_xrange[1], 100)
+        self.m_ys = self.m_xs *self.m_slope + self.m_intercept
+        return np.max(self.m_ys)
 
 if __name__ == "__main__":
     app = QApplication()
     engine = QQmlApplicationEngine()
     model = Model()
+    qmlRegisterType(Model, "Tutorial", 1, 0, "Model")
 
     ctx = engine.rootContext()
-    ctx.setContextProperty("Model", model)
     engine.load('view.qml')
 
     app.exec()
--- view.qml
+++ view.qml
@@ -1,16 +1,27 @@
 import QtQuick
 import QtQuick.Controls
+import Tutorial
 
 ApplicationWindow{
     visible: true
     title: "Fitter"
+
+    Model {
+        id: model
+        intercept: intercept.text
+    }
+
     Rectangle {
         id: main
         anchors.fill: parent
         color: "green"
 
+        TextField {
+            id: intercept
+        }
+
         Text {
-            text: Model.slope + "," + Model.intercept
+            text: model.y_min + "," + model.y_max
             anchors.centerIn: main
         }
     }
